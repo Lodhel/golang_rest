bd26693bdc59620b617542da34adf320d2ad11af
Lodhel <lodhelnew@gmail.com>
2020-06-24 21:25:10 +0300

    structure of rest application

--- book.go
+++ book.go
@@ -1,79 +1,28 @@
 package book
 
 import (
-  "github.com/gorilla/mux"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
   "net/http"
-  "math/rand"
-  "strconv"
   "encoding/json"
+  "fmt"
 )
 
 
-type Book struct {
-  ID      string `json:"id"`
-  Title   string `json:"title"`
-  Author *Author `json:"author"`
+type User struct {
+  gorm.Model
+  Name   string 
+  Soname  string 
 }
 
 type Author struct {
-  Firstname string `json:"firstname"`
-  Lastname  string `json:"lastname"`
+  Firstname string 
+  Lastname  string
 }
 
-
-var books []Book
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
+func CreateUser(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for _, item := range books {
-       if item.ID == params["id"] {
-          json.NewEncoder(w).Encode(item)
-          return
-        }
-    }
-   json.NewEncoder(w).Encode(&Book{})
-}
-
-func createBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    var book Book
-    _ = json.NewDecoder(r.Body).Decode(&book)
-    book.ID = strconv.Itoa(rand.Intn(1000000))
-    books = append(books, book) 
-    json.NewEncoder(w).Encode(book)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for index, item := range books {
-        if item.ID == params["id"] {
-            books = append(books[:index], books[index+1:]...)
-            var book Book
-            _ = json.NewDecoder(r.Body).Decode(&book)
-            book.ID = params["id"]
-            books = append(books, book) 
-            json.NewEncoder(w).Encode(book)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(books)
-}
-
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for index, item := range books {
-        if item.ID == params["id"] {
-            books = append(books[:index], books[index+1:]...)
-            break
-        }
-    }
-    json.NewEncoder(w).Encode(books)
+    var user User
+    _ = json.NewDecoder(r.Body).Decode(&user)
+    json.NewEncoder(w).Encode(user)
 }
--- main.go
+++ main.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-  "log"
-  "fmt"
-  "github.com/lodhel/golang_rest/book"
-    //"github.com/gorilla/mux"
-   //  _ "github.com/lib/pq"
-   // _ "github.com/mattn/go-sqlite3"
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/mysql"
+
+  "github.com/gorilla/mux"
+  "net/http"
+  "log"
+  "fmt"
 )
 
+import k "Lodhel/tests"
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) string {
     fmt.Fprint(w, "Index Page")
+
+    return ""
 }
 
 func main() {
-  db, err := gorm.Open("mysql", "root:123456@/test?charset=utf8&parseTime=True&loc=Local")
-
-  if err != nil {
-    panic(err)
-  } 
-  defer db.Close()
     r := mux.NewRouter()
-    books = append(book.books, book.Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
-    books = append(book.books, book.Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
-    r.HandleFunc("/books", getBooks).Methods("GET")
-    r.HandleFunc("/books/{id}", getBook).Methods("GET")
-    r.HandleFunc("/books", createBook).Methods("POST")
-    r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-    r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+    db, err := gorm.Open("mysql", "root:123456@/test?charset=utf8")
+    if err != nil {
+      panic(err)
+    }
+
+    db.Debug().AutoMigrate(&k.User{})
+    fmt.Println("connection")
+
+    r.HandleFunc("/user", k.CreateUser).Methods("POST")
     log.Fatal(http.ListenAndServe(":8000", r))
 }
