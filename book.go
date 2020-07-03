3d1999ac09579a98dca3a6cb22a5d7da40ed3e0f
Lodhel <lodhelnew@gmail.com>
2020-07-03 21:07:07 +0300

    save data to db

--- book.go
+++ book.go
@@ -4,7 +4,7 @@ import (
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/mysql"
   "net/http"
-  "encoding/json"
+
   "fmt"
 )
 
@@ -21,8 +21,18 @@ type Author struct {
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+    name := r.FormValue("name")
+    soname := r.FormValue("soname")
+
+    fmt.Println(name)
+    db, err := gorm.Open("mysql", "root:123456@/test?charset=utf8")
+    if err != nil {
+      panic(err)
+    }
+
+    _ = db.Exec("insert into test.users (name, soname) values (?, ?)", 
+    name, soname)
+
     w.Header().Set("Content-Type", "application/json")
-    var user User
-    _ = json.NewDecoder(r.Body).Decode(&user)
-    json.NewEncoder(w).Encode(user)
 }
