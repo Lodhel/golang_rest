a019aff060aeb2ed4a70c4001f8b62a611706f96
Lodhel <lodhelnew@gmail.com>
2020-08-09 21:23:48 +0300

    change code for book file

--- book.go
+++ book.go
@@ -8,11 +8,14 @@ import (
   "fmt"
   "encoding/json"
   "log"
+  "regexp"
+  "errors"
 )
 
 
 type User struct {
   gorm.Model
+  Email string `json:"email" validate:"required,email"`
   Name   string 
   Soname  string 
 }
@@ -22,24 +25,48 @@ type Author struct {
   Lastname  string
 }
 
+type ErrorResponse struct {
+  response string
+}
+
+var (
+  badformat = errors.New("invalid format")
+  emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+func serilizer(email string, name string, soname string) {
+  valide := emailRegexp.MatchString(email)
+  request := ErrorResponse{response: "error"}
+  data, err := json.Marshal(request)
+  if err != nil {
+      fmt.Println("bad request")
+    }
+  if valide == true {
+      fmt.Println(data)
+  }
+}
+
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 
+    email := r.FormValue("email")
     name := r.FormValue("name")
     soname := r.FormValue("soname")
 
-    fmt.Println(name)
     db, err := gorm.Open("mysql", "root:123456@/test?charset=utf8")
     if err != nil {
       panic(err)
     }
-    user := User{Name: name, Soname: soname}
+    user := User{Email: email, Name: name, Soname: soname}
+
+    serilizer(email, name, soname)
 
     instance := db.Create(&user)
+
     data, err := json.Marshal(instance)
     if err != nil {
         log.Println(err)
     }
-
+    
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Authorization", "token")
     w.WriteHeader(http.StatusOK)
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module book
 go 1.14
 
 require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.13
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/spf13/viper v1.7.0
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 )
